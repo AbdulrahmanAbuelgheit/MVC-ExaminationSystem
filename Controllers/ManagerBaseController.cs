5cb31b838714491bc1283b565a8d4d31fb5ea437
AbdulrhmannAhmedd <abdulrhmann.ahmedd@gmail.com>
2025-05-13 01:11:12 +0300

    merge from last push

---
+++ Controllers/ManagerBaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExaminationSystemMVC.Controllers
+{
+    public class ManagerBaseController : Controller
+    {
+        //ManagerBaseController()
+        //{
+        //    // Base constructor logic if needed
+        //}
+    }
+}
