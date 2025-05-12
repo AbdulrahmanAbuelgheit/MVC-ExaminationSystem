173025c28f56753533c3e6ccd39ad56e967e61f1
AbdulrhmannAhmedd <abdulrhmann.ahmedd@gmail.com>
2025-05-13 01:31:21 +0300

    saving

--- obj/Debug/net9.0/ExaminationSystemMVC.AssemblyInfo.cs
+++ obj/Debug/net9.0/ExaminationSystemMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExaminationSystemMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7def7387ab42385bd9625b714161ac8cb55f76c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cbe0c35e6690dedf568204f96613cb3e3a0d484")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExaminationSystemMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExaminationSystemMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
