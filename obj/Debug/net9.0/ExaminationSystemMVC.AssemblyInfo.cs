e1b1ca403337cda1c96dfffc577328d1a6726b99
Rawan Labeb <rawan.w.labeb@gmail.com>
2025-05-20 23:04:23 +0300

    add ExamQuestionsWithOptions report

--- obj/Debug/net9.0/ExaminationSystemMVC.AssemblyInfo.cs
+++ obj/Debug/net9.0/ExaminationSystemMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExaminationSystemMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5981f63fcebde5e0f1eab31d887acd75a250da1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38202c0b62280c80f76a3335ea5610d9dac0c87f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExaminationSystemMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExaminationSystemMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
